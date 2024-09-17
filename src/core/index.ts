25ab6fcae5bc45b7b1c4edcfc751b034c33a61a3
yarielre <info@opends.io>
2024-09-17 17:10:44 +0200

    Add enum exports

--- src/core/index.ts
+++ src/core/index.ts
@@ -8,3 +8,11 @@ export * from "./context";
 export * from "./seo";
 export * from "./Menu";
 export * from "./Table";
+
+export * from "./enums/Actions";
+export * from "./enums/AuthError";
+export * from "./enums/BaseError";
+export * from "./enums/FrequencyRepeatEnum";
+export * from "./enums/Language";
+export * from "./enums/MediaEntityTypeEnum";
+export * from "./enums/RolEntityEnum";
